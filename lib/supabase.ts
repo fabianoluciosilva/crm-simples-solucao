f97d396f31cc123f780de4bdd33d5588d713a05d
fabianoluciosilva <fabianoluciosilva@gmail.com>
2026-04-05 09:47:25 -0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,10 +3,12 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-// A opção auth: { persistSession: true } avisa ao navegador para guardar o login
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
+    detectSessionInUrl: true,
+    // Força o uso do armazenamento do navegador para não perder a sessão no F5
+    storage: typeof window !== 'undefined' ? window.localStorage : undefined,
   }
 });
